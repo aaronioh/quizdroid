d56a9efc799f3d0083855bbeb78f376155d43d6b
Aaron Oh <aaronioh@D-69-91-185-149.dhcp4.washington.edu>
2018-04-23 00:44:14 -0700

    topic description activity

---
+++ app/src/main/java/edu/washington/aaronioh/quizdroid/TopicDescription.kt
@@ -0,0 +1,32 @@
+package edu.washington.aaronioh.quizdroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class TopicDescription : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topic_description)
+
+        val topic = intent.getStringExtra("topic")
+        val description = intent.getStringExtra("description")
+        val questions = intent.getStringArrayExtra("questions")
+        val num = questions.size
+
+        val textTopic = findViewById<TextView>(R.id.textTopic) as TextView
+        val textDesc = findViewById<TextView>(R.id.textDesc) as TextView
+        val textNumQuestions = findViewById<TextView>(R.id.textNumQuestions) as TextView
+        val buttonBegin = findViewById<Button>(R.id.buttonBegin) as Button
+
+        textTopic.text = topic
+        textDesc.text = description
+        textNumQuestions.text = "Number of questions: " + num
+
+        buttonBegin.setOnClickListener {
+            //val intent = Intent(this, )
+        }
+    }
+}
