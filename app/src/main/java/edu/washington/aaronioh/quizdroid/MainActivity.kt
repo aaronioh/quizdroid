6d828f91eeafb51ca32a64db93c9ac78bacb0d4d
Aaron Oh <aaronioh@D-69-91-185-149.dhcp4.washington.edu>
2018-05-15 02:18:46 -0700

    everything but downloading to sd card

--- app/src/main/java/edu/washington/aaronioh/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/aaronioh/quizdroid/MainActivity.kt
@@ -15,21 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
-        val app = QuizApp
-        val topics = app.instance.getTopics()
+        if (!OnlineRepository().topics.isEmpty()) {
+            val app = QuizApp
+            val topics = app.instance.getTopics()
 
-        val listView = findViewById<ListView>(R.id.listViewTopics) as ListView
+            val listView = findViewById<ListView>(R.id.listViewTopics) as ListView
 
-        val topicsList = listOf(topics[0].title, topics[1].title, topics[2].title)
+            val topicsList = listOf(topics[0].title, topics[1].title, topics[2].title)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, topicsList)
-        listView.adapter = adapter
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, topicsList)
+            listView.adapter = adapter
 
-        listView.setOnItemClickListener { _, _, position, id ->
-            val intent = Intent(this, QuizFragment::class.java).apply {
-                putExtra("topicPos", position)
+            listView.setOnItemClickListener { _, _, position, id ->
+                val intent = Intent(this, QuizFragment::class.java).apply {
+                    putExtra("topicPos", position)
+                }
+                startActivity(intent)
             }
-            startActivity(intent)
         }
     }
 
