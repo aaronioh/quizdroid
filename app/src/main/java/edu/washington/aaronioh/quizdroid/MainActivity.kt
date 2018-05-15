cd36bea8f7ee7724b87f48ca4a4666269bedf53a
Aaron Oh <aaronioh@D-69-91-185-149.dhcp4.washington.edu>
2018-05-15 02:07:26 -0700

    Revert "everything but download to sd card working"
    This reverts commit fd8812c26a26c34061f3584f5d5a6a4e0137804d.

--- app/src/main/java/edu/washington/aaronioh/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/aaronioh/quizdroid/MainActivity.kt
@@ -1,10 +1,8 @@
 package edu.washington.aaronioh.quizdroid
 
-import android.Manifest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
@@ -17,25 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+        val app = QuizApp
+        val topics = app.instance.getTopics()
 
-        if (!OnlineRepository().topics.isEmpty()) {
-            val app = QuizApp
-            val topics = app.instance.getTopics()
+        val listView = findViewById<ListView>(R.id.listViewTopics) as ListView
 
-            val listView = findViewById<ListView>(R.id.listViewTopics) as ListView
+        val topicsList = listOf(topics[0].title, topics[1].title, topics[2].title)
 
-            val topicsList = listOf(topics[0].title, topics[1].title, topics[2].title)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, topicsList)
+        listView.adapter = adapter
 
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, topicsList)
-            listView.adapter = adapter
-
-            listView.setOnItemClickListener { _, _, position, id ->
-                val intent = Intent(this, QuizFragment::class.java).apply {
-                    putExtra("topicPos", position)
-                }
-                startActivity(intent)
+        listView.setOnItemClickListener { _, _, position, id ->
+            val intent = Intent(this, QuizFragment::class.java).apply {
+                putExtra("topicPos", position)
             }
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/edu/washington/aaronioh/quizdroid/OnlineRepository.kt
+++ app/src/main/java/edu/washington/aaronioh/quizdroid/OnlineRepository.kt
@@ -1,21 +1,18 @@
 package edu.washington.aaronioh.quizdroid
 
 import android.os.AsyncTask
-import android.os.Environment
 import org.json.JSONArray
 import java.io.BufferedInputStream
-import java.io.File
-import java.io.FileInputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
 class OnlineRepository : TopicRepository, AsyncTask<String, String, String> {
     val topics = ArrayList<Topic>()
-    var filename = ""
+    var url = ""
 
     constructor() {
-        filename = "/Download/questions.json"
-        this.execute(filename).get()
+        url = "http://tednewardsandbox.site44.com/questions.json"
+        execute(url).get()
     }
 
     override fun getTopics(): List<Topic> {
@@ -27,43 +24,37 @@ class OnlineRepository : TopicRepository, AsyncTask<String, String, String> {
     }
 
     override fun doInBackground(vararg p0: String?): String {
-        val file = File(Environment.getExternalStorageDirectory().toString() + filename)
-
-        //val connection = URL(url).openConnection() as HttpURLConnection
+        val connection = URL(url).openConnection() as HttpURLConnection
 
         var input = ""
 
-        if (file.exists()) {
-            try {
-                //input = BufferedInputStream(connection.inputStream).use { it.reader().use { reader -> reader.readText() } }
-                input = file.bufferedReader().use { it.readText() }
-            } catch (e : Exception) {
-                e.printStackTrace()
-            }
-
-            val obj = JSONArray(input)
-
-            for (i in 0..(obj.length() - 1)) {
-                val topic = obj.getJSONObject(i)
-                val title = topic.getString("title")
-                val desc = topic.getString("desc")
-                val questions = topic.getJSONArray("questions")
-                val quiz = ArrayList<Quiz>()
-                for (j in 0..(questions.length() - 1)) {
-                    val question = questions.getJSONObject(j)
-                    val text = question.getString("text")
-                    val answer = question.getInt("answer") - 1
-                    val jsonAnswers = question.getJSONArray("answers")
-                    val answers = Array(jsonAnswers.length()) { "" }
-                    for (k in 0..(jsonAnswers.length() - 1)) {
-                        answers[k] = jsonAnswers[k].toString()
-                    }
-                    quiz.add(Quiz(text, answers, answer))
-                }
-                topics.add(Topic(title, desc, desc, quiz))
-            }
+        try {
+            input = BufferedInputStream(connection.inputStream).use { it.reader().use { reader -> reader.readText() } }
+        } finally {
+            connection.disconnect()
         }
 
+        val obj = JSONArray(input)
+
+        for (i in 0..(obj.length() - 1)) {
+            val topic = obj.getJSONObject(i)
+            val title = topic.getString("title")
+            val desc = topic.getString("desc")
+            val questions = topic.getJSONArray("questions")
+            val quiz = ArrayList<Quiz>()
+            for (j in 0..(questions.length() - 1)) {
+                val question = questions.getJSONObject(j)
+                val text = question.getString("text")
+                val answer = question.getInt("answer") - 1
+                val jsonAnswers = question.getJSONArray("answers")
+                val answers = Array(jsonAnswers.length()) { "" }
+                for (k in 0..(jsonAnswers.length() - 1)) {
+                    answers[k] = jsonAnswers[k].toString()
+                }
+                quiz.add(Quiz(text, answers, answer))
+            }
+            topics.add(Topic(title, desc, desc, quiz))
+        }
         return input
     }
 }
